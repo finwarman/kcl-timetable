5b6ef48d7d09a31e52b7a85b0570b631ca6faa5c
Fin <finleywarman@hotmail.co.uk>
2020-09-30 09:24:49 +0100

    Add command line args, and UIDs to prevent duplicate imports

--- kclical.py
+++ kclical.py
@@ -13,41 +13,59 @@ import os
 import re
 import keyring
 import requests
-import crayons
 from icalendar import Calendar, Event
+import argparse
 
+parser = argparse.ArgumentParser(
+    description='Download your KCL timetable as a calendar on the command line.')
+parser.add_argument('-r', '--reset',
+                    action="store_true", dest="reset",
+                    help="Reset your login credentials", default=False)
+parser.add_argument('--reverse',
+                    action="store_true", dest="reverse",
+                    help="Print timetable dates in reverse order", default=False)
+parser.add_argument('-d', '--days', nargs='?', default='300', const='const',
+                    help="The number of days to look ahead")
+
+args = parser.parse_args()
+
+days = 300
+if args.days.isnumeric():
+    days = int(args.days)
+else:
+    exit("'days' parameter must be int")
 
 # keyring namespace for this app
 SERVICE_ID = 'kcl_timetable'
 KNUM_REG_PATTERN = re.compile("[k|K]\d{7}")
 
-file_path = "knumber.fin"
-if not os.path.isfile(file_path):
-    open("knumber.fin", "w").close()
+if (args.reset):
+    try:
+        keyring.delete_password(SERVICE_ID, "knumber")
+    except:
+        print("No K-Number stored")
 
-with open("knumber.fin", "r+") as unamefile:
-    uname = unamefile.read()
+    try:
+        keyring.delete_password(SERVICE_ID, "password")
+    except:
+        print("No password stored")
 
-    if(bool(KNUM_REG_PATTERN.match(uname)) == False):
-        print("No K-Number Saved.")
 
-        while bool(KNUM_REG_PATTERN.match(uname)) == False:
-            print("Please enter a valid K-Number:")
-            uname = input()
+knum = keyring.get_password(SERVICE_ID, "knumber")
 
-        unamefile.write(uname)
+if knum == None:
+    knum = ""
+    while not re.match(KNUM_REG_PATTERN, knum):
+        knum = input("Enter a valid K-Number: ")
+    pwd = getpass("Enter your password: ")
+    keyring.set_password(SERVICE_ID, "knumber", knum)
+    keyring.set_password(SERVICE_ID, "password", pwd)
 
-        pwd = getpass("Enter your password: ")
-        keyring.set_password(SERVICE_ID, uname, pwd)
+password = keyring.get_password(SERVICE_ID, "password")  # retrieve password
 
-unamefile.close()
-
-password = keyring.get_password(SERVICE_ID, uname)  # retrieve password4
-
-UNAME = uname
+UNAME = knum
 PASSWD = password
 
-# STARTDATE = (datetime.utcnow() - timedelta(days=1)).isoformat()
 STARTDATE = (datetime.utcnow().replace(
     hour=0, minute=0, second=0, microsecond=0)).isoformat()
 ENDDATE = (datetime.utcnow() + timedelta(days=240)).isoformat()
@@ -74,17 +92,6 @@ APP_REQ_HEADERS = {
     'Connection':	    'keep-alive'
 }
 
-'''
-    Authorization:	Basic YXBwbGljYXRpb25fc2VjX3VzZXI6ZjJnaDUzNDg=
-
-    This is base64 encoded, for HTTP 'Basic' authentication scheme in headers,
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
-    When decoded it reads:
-    application_sec_user:f2gh5348
-
-    I'd like to try another user login to see if this token is unique?
-'''
-
 XML_RESPONSE = ""
 try:
     XML_RESPONSE = requests.post(
@@ -100,7 +107,6 @@ except:
         print("Unable to connect - check internet connection and try again?")
         sys.exit(0)
 
-
 ROOT = ET.fromstring(XML_RESPONSE)
 CALITEMS = (list(list(ROOT)[0]))
 
@@ -114,11 +120,11 @@ for item in CALITEMS:
 
     date_time_str = calentry['end']
     date_time_obj = datetime.fromisoformat(date_time_str)
-    calentry['end'] = date_time_obj
+    calentry['end'] = date_time_obj.strftime('%H:%M')
 
     date_time_str = calentry['start']
     date_time_obj = datetime.fromisoformat(date_time_str)
-    calentry['start'] = date_time_obj
+    calentry['start'] = date_time_obj.strftime('%H:%M')
 
     date_key = date_time_obj.strftime("%Y-%m-%d")
 
@@ -132,10 +138,25 @@ for item in CALITEMS:
         calentry['type'] = 'Lecture'
     elif('/SmG/' in calentry['desc1']):
         calentry['type'] = 'Small Group Tut.'
+    elif('/DROP IN/' in calentry['desc1']):
+        calentry['type'] = 'Drop-In Session'
     else:
         calentry['type'] = 'Lesson'
 
+    if('/' in calentry['desc1']):
+        m = re.search(r" \w*\/", calentry['desc1'])
+        calentry['desc1'] = calentry['desc1'][:m.start()]
+
+    if(calentry['desc2'] == None):
+        calentry['desc2'] = calentry['desc1']
+
     dates.setdefault(date_key, []).append(calentry)
+
+if len(dates.keys()) < 1:
+    print("\n[No Events Found]\n")
+else:
+    print("\nExported timetable for the next {} days:".format(days))
+
 
 cal = Calendar()
 
@@ -145,13 +166,32 @@ for key in sorted(dates.keys(), reverse=True):  # Top to bottom flag
 
     for row in events:
         event = Event()
+
+        start = datetime.combine(
+            dt, datetime.strptime(row.get('start', '08:00'), '%H:%M').time()
+        )
+        end = datetime.combine(
+            dt, datetime.strptime(row.get('end', '20:00'), '%H:%M').time()
+        )
+
+        event.add('dtstart', start)
+        event.add('dtend', end)
+
         event.add('description', row['desc1']+' - ' +
                   row.get('teacherName', 'Teaching Assistant'))
-        event.add('dtstart', row['start'])
-        event.add('dtend', row['end'])
         # event.add('summary', row['type']+' - '+row['desc2'])
         event.add('summary', row.get('desc2', 'Description'))
-        event.add('location', row['locAdd1'])
+
+        # location data: locAdd1, locCode
+        event.add('location', row.get(
+            'locAdd1', 'Online/Remote' if 'locCode' not in row else ''))
+
+        uid = '{}{}{}{}'.format(start, end, event.get(
+            'location'), event.get('description'))
+        uid = ''.join(c for c in uid if (c.isalnum()
+                                         or c in '_-'))  # strip to alphanum/_/-
+        event.add('uid', uid)
+
         cal.add_component(event)
 
 desktop = os.path.expanduser("~/Desktop")
